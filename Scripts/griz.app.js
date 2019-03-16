96ea35ffaf0816ddfa972324b72cde1e0e5fe15e
Ryan English <ryeenglish9@gmail.com>
2019-03-16 19:41:18 -0400

    did stuff

--- Scripts/griz.app.js
+++ Scripts/griz.app.js
@@ -28,7 +28,7 @@ const skills = [
     },
     {
         name: 'Java',
-        level: levels[1],
+        level: levels[2],
         tag: programming.name
     },
     {
@@ -38,7 +38,7 @@ const skills = [
     },
     {
         name: 'PostgreSQL',
-        level: levels[2],
+        level: levels[1],
         tag: programming.name
     },
     {
@@ -72,6 +72,11 @@ const skills = [
         tag: [programming.name, game.name]
     },
     {
+        name: 'PHPStorm',
+        level: levels[2],
+        tag: app.name
+    },
+    {
         name: 'VS Code',
         level: levels[2],
         tag: app.name
@@ -85,6 +90,11 @@ const skills = [
         name: 'IntelliJ',
         level: levels[2],
         tag: app.name
+    },
+    {
+        name: 'Use Cases',
+        level: levels[2],
+        tag: other.name
     },
     {
         name: 'Support',
@@ -165,6 +175,21 @@ const skills = [
         name: 'Unity',
         level: levels[0],
         tag: [game.name]
+    },
+    {
+        name: 'Symfony',
+        level: levels[1],
+        tag: [libraries.name]
+    },
+    {
+        name: 'PHP',
+        level: levels[1],
+        tag: [programming.name]
+    },
+    {
+        name: 'SASS',
+        level: levels[1],
+        tag: [app.name]
     }
 ];
 
@@ -191,7 +216,7 @@ const projects = [
                 name: 'View Code on Github'
             },
             {
-                href: '/project-space',
+                href: '/files/projects/project-space/index.html',
                 name: 'View Project Space'
             },
         ],
@@ -233,21 +258,42 @@ const projects = [
         ]
     },
     {
-        name: 'University of Louisville E-Commerce',
+        name: 'University of Louisville E-Commerce Website',
         links: [
             {
                 href: 'https://github.com/GrizzlyEnglish/cis-420-ecommerce',
                 name: 'View Code on Github'
+            },
+            {
+                href: '/files/projects/e-commerce/CreateOperatorUseCase.pdf',
+                name: 'Create Operator Account Use Case'
+            },
+            {
+                href: '/files/projects/e-commerce/CreatePaymentUseCase.pdf',
+                name: 'Create Payment Use Case'
+            },
+            {
+                href: '/files/projects/e-commerce/CreateMarketingCampaignUseCase.pdf',
+                name: 'Create Marketing Campaign Use Case'
+            },
+            {
+                href: '/files/projects/e-commerce/EditOrderUseCase.pdf',
+                name: 'Edit Order Use Case'
+            },
+            {
+                href: '/files/projects/e-commerce/Bracyl Prototypes_i5.pdf',
+                name: 'Prototype Presentation'
             }
         ],
         lines: [
             'Bakery E-Commerce built for Sweet Karoline Cakes',
             'Features Gallery, Product Page, Customer Portal, and Admin Portal',
+            'Expresses knowledge writing Use Cases and Prototyping',
             'VueJS, ExpressJS, SQLServer Stack'
         ]
     },
     {
-        name: 'University of Louisville Degree Work Collection',
+        name: 'University of Louisville Degree Code Collection',
         links: [
             {
                 href: 'https://github.com/GrizzlyEnglish/uofl-cis-collection',
@@ -260,6 +306,53 @@ const projects = [
             'Expresses knowledge in LINQ',
             'Expresses knowlege in application development',
             'Built in C#'
+        ]
+    },
+    {
+        name: 'University of Louisville Degree Data Mining',
+        links: [
+            {
+                href: '/files/projects/datamining/Tutorial_1_Report_Ryan_English.pdf',
+                name: 'SAS Tutorial Report'
+            },
+            {
+                href: '/files/projects/datamining/Tutorial2_Report_English_Ryan.pdf',
+                name: 'SAS Tutorial 2 Report'
+            },
+            {
+                href: '/files/projects/datamining/Project4_Ryan_English.pdf',
+                name: 'SAS House Price Mining Report'
+            }
+        ],
+        lines: [
+            'A couple of data mining reports, mining data with SAS',
+            'Expresses knowledge in SASS and data mining techniques'
+        ]
+    },
+    {
+        name: 'University of Louisville Case Studies',
+        links: [
+            {
+                href: '/files/projects/cases/CaseFour_Webvan_2.28.19.pdf',
+                name: 'Webvan Case'
+            },
+            {
+                href: '/files/projects/cases/CaseOne_BNAres_REnglish_1_26_2018.pdf',
+                name: 'Burlington Northern ARES Case'
+            },
+            {
+                href: '/files/projects/cases/CaseThree_Symantec_REnglish_2_19_2019.pdf',
+                name: 'Symantec Case'
+            },
+            {
+                href: '/files/projects/cases/CaseTwo_Appex_REnglish_2_2_2019.pdf',
+                name: 'Appex Case'
+            }
+        ],
+        lines: [
+            'Case Studies on business practices and examples of the internet enabled business model',
+            'Each case examines an organization issue and recommends a solution and offers rejected solutions',
+            'Expresses knowledge in Technical Writing'
         ]
     },
     {
